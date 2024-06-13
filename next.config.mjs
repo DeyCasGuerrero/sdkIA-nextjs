c0c73ad7a8c789346ae9365a3a2293e845386f96
DeyCasGuerrero <depordey@gmail.com>
2024-06-13 13:50:31 -0500

    Begining of screaming architecture

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',      // Utiliza el protocolo HTTPS.
+                hostname: 'a.storyblok.com', // Permite el dominio 'a.storyblok.com'.
+                port: '',               // El puerto se deja vacío para usar el puerto por defecto (443 para HTTPS).
+                pathname: '/f/274239/**', // Permite cualquier subruta dentro de '/f/274239/'.
+            },
+
+        ],
+    },
+};
 
 export default nextConfig;
